14b541251dd05b0f5a14bd7526253fb4bbb5a1ea
liling <vicki.li1109@icloud.com>
2016-09-27 02:56:52 +0800

    init project

---
+++ app/src/main/java/com/example/sunfiyes/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sunfiyes.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sunfiyes on 2016/9/27 0027.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
