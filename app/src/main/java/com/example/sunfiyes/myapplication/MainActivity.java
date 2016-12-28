1d61564d4843505b99b585d2dd00f545521957c7
liling <vicki.li1109@icloud.com>
2016-12-28 17:08:30 +0800

    12.28

--- app/src/main/java/com/example/sunfiyes/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sunfiyes/myapplication/MainActivity.java
@@ -49,6 +49,7 @@ import com.baidu.location.Poi;
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
     private ImageView mUpdateaBtn;//图标控件
     private ImageView mCitySelect;
+    private ImageView mCityLocation;//定位按钮
 
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv, temperature_range_Tv, climateTv, windTv, city_name_Tv;
     private ImageView weatherImg, pmImg;
@@ -69,6 +70,9 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
         mUpdateaBtn = (ImageView) findViewById(R.id.title_update_btn);
         mUpdateaBtn.setOnClickListener(this);
+
+        mCityLocation = (ImageView)findViewById(R.id.title_location);
+        mCityLocation.setOnClickListener(this);
 
         if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
             Log.d("myWeather", "网络OK");
@@ -96,11 +100,45 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式
         option.setOpenGps(true);
         option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度，默认值gcj02
-        option.setScanSpan(500000);// 设置发起定位请求的间隔时间为5000ms
+        option.setScanSpan(1000);// 设置发起定位请求的间隔时间为5000ms
         option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+        option.setAddrType("all");
         mLocationClient.setLocOption(option);
-        //未完待续。。。
 
+        mLocationClient.registerLocationListener(new BDLocationListener() {
+             @Override
+             public void onReceiveLocation(BDLocation location) {
+                 if (location == null)
+                     return;
+                 StringBuffer sb = new StringBuffer(256);
+                 sb.append("time : ");
+                 sb.append(location.getTime());
+                 sb.append("\nerror code : ");
+                 sb.append(location.getLocType());
+                 sb.append("\nlatitude : ");
+                 sb.append(location.getLatitude());
+                 sb.append("\nlontitude : ");
+                 sb.append(location.getLongitude());
+                 sb.append("\nradius : ");
+                 sb.append(location.getRadius());
+                 sb.append("\ncity : ");
+                 sb.append(location.getCity());
+                 sb.append("\nAdress : ");
+                 sb.append(location.getAddress().address);
+                 if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                     sb.append("\nspeed : ");
+                     sb.append(location.getSpeed());
+                     sb.append("\nsatellite : ");
+                     sb.append(location.getSatelliteNumber());
+                 } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                     sb.append("\naddr : ");
+                     sb.append(location.getAddrStr());
+                 }
+                 Log.i("myWeather", sb.toString());
+             }
+         }
+        );
+        mLocationClient.start();
     }
 
     private Handler mHandler = new Handler() {
@@ -167,6 +205,21 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
             Intent i = new Intent(this, SelectCity.class);
             startActivityForResult(i, 1);
+        }
+
+        if(view.getId()==R.id.title_location)
+        {
+            if (mLocationClient == null) {
+                return;
+            }
+            Toast.makeText(this, "正在进行城市定位!", Toast.LENGTH_SHORT).show();
+            Log.i("myWeather", "-> t_base_test");
+            if (mLocationClient.isStarted()) {
+                mLocationClient.stop();
+            } else {
+                mLocationClient.start();
+                mLocationClient.requestLocation();
+            }
         }
 
     }
